3c0b25c6367914050f69d3abde9d8da522aded7c
Andriy Veres <andriy-pilot@ukr.net>
2019-01-02 22:17:01 +0200

    Added preliminary quantum memory portion allocation. Need to a lot of code and some tests.

--- scull/development/scull.c
+++ scull/development/scull.c
@@ -30,11 +30,19 @@ MODULE_DESCRIPTION("Simple character device driver for write and read character 
 
 /* scull work file_operations functions for work with driver, realized minimum quantity need for work */
 /* TODO: place it into dedicated .c/.h files need update build system, how? */
+
+/* SCULL interface */
 int     scull_trim(struct scull_dev * dev);
 int     scull_open(struct inode *inode, struct file *filp);
 int     scull_release(struct inode *inode, struct file *filp);
 ssize_t scull_read(struct file *filp, char *buf, size_t count, loff_t *f_pos);
 ssize_t scull_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos);
+
+/*SCULL helpers functions */
+int scull_qset_alloc(struct scull_dev * dev);
+int scull_qset_free(struct scull_dev * dev);
+
+
 
 static int scull_init(void)
 {
@@ -80,11 +88,24 @@ static int scull_init(void)
 	}
 #endif /* SCULL_DEBUG */
 
+	/* Set  scull memory configuration */
+	/*TODO: realize with module param */
+	scull_dev.quantum_count = SCULL_QUANTUM_COUNT;
+	scull_dev.quantum_size = SCULL_QUANTUM_SIZE;
+	scull_dev.qset_head = NULL;
+	scull_dev.qset_size = SCULL_QSET_SIZE;
+
+	/* Allocation scull linked list */
+	scull_qset_alloc(&scull_dev);
+
+	
+
 	/* Filling scull file_operations */
-	scull_fops.owner = THIS_MODULE;
-	scull_fops.open  = scull_open;
-	scull_fops.read  = scull_read;
-	scull_fops.write = scull_write;
+	scull_fops.owner   = THIS_MODULE;
+	scull_fops.open    = scull_open;
+	scull_fops.read    = scull_read;
+	scull_fops.write   = scull_write;
+	scull_fops.release = scull_release;
 
 	/* initialize number of device opens counter */
 	scull_dev.number_opens = 0;
@@ -111,18 +132,26 @@ static void scull_exit(void)
 
 	/* free memory, allocated for characted buffer */
 #ifdef SCULL_DEBUG
-	printk(KERN_DEBUG "SCULL: free memory allocated to character buffer...\n");
+	printk(KERN_DEBUG "SCULL: free memory, allocated for character buffer at address:%X...\n", scull_dev.buffer);
 #endif /* SCULL_DEBUG */
-	if(scull_dev.buffer != NULL)
-	{
+	if(scull_dev.buffer != NULL) {
 		kfree(scull_dev.buffer);
+		scull_dev.buffer = NULL;
+#ifdef SCULL_DEBUG
+		printk(KERN_DEBUG "SCULL: success free memory, allocated for character buffer\n");
+#endif /* SCULL_DEBUG */
 	}
 #ifdef SCULL_DEBUG
-	else
-	{
-		printk(KERN_ALERT "SCULL: empty pointer to character buffer\n");
+	else {
+		printk(KERN_ALERT "SCULL: empty pointer to character \n");
 	}
 #endif /* SCULL_DEBUG */
+
+	/* free memory, allocated for scull linked list */
+#ifdef SCULL_DEBUG
+	printk(KERN_DEBUG "SCULL: try to free memory, allocated for qset that stored in %X...\n",&scull_dev);	
+#endif /* SCULL_DEBUG */
+	scull_qset_free(&scull_dev);
 
 
 	/* Character device number deregistration */
@@ -140,6 +169,27 @@ int scull_trim(struct scull_dev * dev)
 #ifdef SCULL_DEBUG
 	printk(KERN_DEBUG "SCULL: debug, %s %d\n",__func__,__LINE__);
 #endif /* SCULL_DEBUG */
+
+	/* pointer to next node in linked list, scull_qset`s, pointer to current node */
+	struct scull_qset * p_next = dev->qset_head->p_next,* p_curr = dev->qset_head;
+	unsigned int qset_size = dev->qset_size;
+	unsigned int i;
+
+	if(p_curr == NULL) {
+		/* TODO: update when bad case */
+	}
+
+	/* Walk through linked list */
+	do {
+		/* zero memory, allocated for quantums in qset */
+		for(i=0;i<dev->quantum_count;++i) {
+			memset(p_curr->data[i],0,dev->quantum_size);
+		}
+		p_next = p_curr->p_next;
+		p_curr = p_next;
+	} while(p_next != NULL);
+	
+	
 	return 0;
 }
 
@@ -152,7 +202,20 @@ int scull_open(struct inode *inode, struct file *filp)
 	printk(KERN_DEBUG "SCULL: debug, %s %d number_opens:%u\n",__func__,__LINE__,scull_dev.number_opens);
 #endif /* SCULL_DEBUG */
 
-	return 0;  /* success always for now */
+	filp->private_data = &scull_cdev; /* for another scull interface functions */ /* TODO: replace to containter_of macro */
+	/* Set the device lenght zero if device was open only for write */
+#ifdef SCULL_DEBUG
+	printk(KERN_DEBUG "SCULL: debug, %s %d (filp->f_flags %X\n",__func__,__LINE__,filp->f_flags);
+#endif /* SCULL_DEBUG */
+	if ((filp->f_flags & O_ACCMODE) == O_WRONLY) {
+		scull_trim(&scull_dev); /* Replace to incapsuled in something */
+#ifdef SCULL_DEBUG
+		printk(KERN_DEBUG "SCULL: debug, %s %d (filp->f_flags & O_ACCMODE) == O_WRONLY\n",__func__,__LINE__);
+#endif /* SCULL_DEBUG */
+	}
+	/* TODO: check else handling if necessary */
+
+	return 0; /* success always */
 }
 
 
@@ -204,12 +267,12 @@ ssize_t scull_write(struct file *filp, const char *buf, size_t count, loff_t *f_
 	ssize_t result = count;
 
 	/* check position and size according to character buffer size */
-	if(*f_pos + count > SCULL_BUFFER_SIZE) {
+	if(*f_pos + count > scull_dev->quantum_size * scull_dev->quantum_count * scull_dev->qset_size) {
 #ifdef SCULL_DEBUG
-		printk(KERN_ALERT "SCULL: %s %d debug, f_pos + count > SCULL_BUFFER_SIZE\n",__func__,__LINE__);
+		printk(KERN_ALERT "SCULL: %s %d debug, f_pos + count > size of scull allocated memory\n",__func__,__LINE__);
 		printk(KERN_ALERT "SCULL: %s %d debug, *f_pos:%llu\n",__func__,__LINE__,(unsigned long long)*f_pos);
 		printk(KERN_ALERT "SCULL: %s %d debug, count:%llu\n",__func__,__LINE__,(unsigned long long)count);
-		printk(KERN_ALERT "SCULL: %s %d debug, SCULL_BUFFER_SIZE:%d\n",__func__,__LINE__,SCULL_BUFFER_SIZE);
+		printk(KERN_ALERT "SCULL: %s %d debug, size of scull allocated memory:%d\n",__func__,__LINE__,scull_dev->quantum_size * scull_dev->quantum_count * scull_dev->qset_size);
 #endif /* SCULL_DEBUG */
 		return -EFAULT;
 	}
@@ -234,6 +297,116 @@ ssize_t scull_write(struct file *filp, const char *buf, size_t count, loff_t *f_
 #endif /* SCULL_DEBUG */
 
 	return result;
+}
+
+int scull_qset_alloc(struct scull_dev * dev)
+{
+	struct scull_qset * p_qset = NULL, * p_previous = NULL, * p_next = NULL;
+	unsigned int i,j;
+
+	dev->qset_head = NULL; /* Initialize qset_head pointer */
+
+	/* Allocation memory for qset (node in linked list) and create first element in linked list */
+	dev->qset_head =  kzalloc(sizeof(struct scull_qset),GFP_KERNEL);
+
+	if(dev->qset_head == NULL) {
+		/* TODO: process bad case */
+	}
+	
+	dev->qset_head->p_previous = NULL; /* Head of linked list */
+	/* Allocate memory for qset pointers array */
+	dev->qset_head->data = kzalloc(sizeof(void *)*(dev->quantum_count),GFP_KERNEL);
+	for(j = 0;j < dev->quantum_count;++j) {
+		/* Allocating memory for each qset in quantun */
+		dev->qset_head->data[j] = kzalloc(dev->quantum_size,GFP_KERNEL);
+		/* TODO: update bad case */
+	}
+	p_previous = dev->qset_head; /* storing previous element */
+
+	/* Create linked list of qset`s without first element, as it was created above */
+	for(i=1;i<dev->qset_size;++i) {
+		/* Allocation memory for qset (node in linked list) */
+		p_qset =  kzalloc(sizeof(struct scull_qset),GFP_KERNEL);
+
+		if(p_qset == NULL) {
+		/* TODO: process bad case */
+		}
+
+		p_previous->p_next = p_qset;             /* Link from previous node to current node */
+
+		/* Allocate memory for qset pointers array */
+		p_qset->data = kzalloc(sizeof(void *)*(dev->quantum_count),GFP_KERNEL);
+		for(j = 0;j < dev->quantum_count;++j) {
+			/* Allocating memory for each qset in quantun */
+			p_qset->data[j] = kzalloc(dev->quantum_size,GFP_KERNEL);
+			/* TODO: update bad case */
+		}
+		p_previous = p_qset; /* Update pointer to previous node */
+	}
+	p_previous->p_next = NULL; /* End of linked list */
+	return 0; /* Success always for now */
+}
+int scull_qset_free(struct scull_dev * dev)
+{
+	struct scull_qset * p_curr = NULL, * p_previous = NULL, * p_next = NULL;
+	unsigned int i, j;
+
+	/* Check qset_head pointer */
+	if(dev->qset_head == NULL) {
+		/* TODO: process bad case */
+#ifdef SCULL_DEBUG
+		printk(KERN_WARNING "SCULL: %s %d debug, NULL pointer in dev->qset_head\n",__func__,__LINE__);
+#endif /* SCULL_DEBUG */
+	}
+	
+
+	p_curr = dev->qset_head;
+
+	/* Walk through linked list of scull_qset`s */
+	while(p_curr != NULL) {
+#ifdef SCULL_DEBUG /* TODO: update bad case */
+		printk(KERN_DEBUG "SCULL: debug p_curr (%X) linked list iteration \n",p_curr);
+#endif /* SCULL_DEBUG */
+		if(p_curr->data == NULL) {
+#ifdef SCULL_DEBUG /* TODO: update bad case */
+			printk(KERN_WARNING "SCULL: qset with NULL address when try to free\n");
+#endif /* SCULL_DEBUG */
+		}
+
+		/* Free memory for each qset in quantun */
+		for(j = 0;j < dev->quantum_count;++j) {
+#ifdef SCULL_DEBUG
+			printk(KERN_DEBUG "SCULL:debug iteration [%d] in qset`s array\n",j);
+#endif /* SCULL_DEBUG */
+			if(p_curr->data[j] != NULL) {
+				kfree(p_curr->data[j]);
+#ifdef SCULL_DEBUG
+				printk(KERN_DEBUG "SCULL:debug success free p_curr->data[%d] at \n",j,p_curr->data[j]);
+#endif /* SCULL_DEBUG */
+				p_curr->data[j] = NULL;
+			}
+			else {
+#ifdef SCULL_DEBUG	/* TODO: update bad case */
+				printk(KERN_WARNING "SCULL: p_curr->data[%d] is NULL \n",j);	
+#endif /* SCULL_DEBUG */
+			}	
+		}
+		/* Free memory for qset pointers array */
+#ifdef SCULL_DEBUG
+		printk(KERN_WARNING "SCULL: debug try to free p_curr->data at %X\n",p_curr->data);	
+#endif /* SCULL_DEBUG */
+		kfree(p_curr->data);
+#ifdef SCULL_DEBUG
+		printk(KERN_WARNING "SCULL: debug success free p_curr->data at %X\n",p_curr->data);	
+#endif /* SCULL_DEBUG */
+		p_curr->data = NULL;
+			
+		p_previous = p_curr;     /* Update pointer to previous node */
+		p_curr = p_curr->p_next; /* Go to the next node */
+		kfree(p_previous);       /* Free current node */
+	}
+	return 0; /* Success always for now */
+
 }
 
 
--- scull/development/scull.h
+++ scull/development/scull.h
@@ -15,7 +15,19 @@
 #define SCULL_DEV_NAME        "scull"
 #define SCULL_DEV_FIRSTMIRROR 0
 #define SCULL_BUFFER_SIZE     132000 /* size of character buffer */
+#define SCULL_QUANTUM_SIZE    1000     /* default scull quantum size in bytes */
+#define SCULL_QUANTUM_COUNT   4000    /* # of quantums in 1 scull qset linked list */
+#define SCULL_QSET_SIZE       42   /* default quantity of nodes in linked list squll_qset */
 
+/*
+ * Struct scull_qset realize scull memory utilization via linked list of nodes (struct scull_qset), each oh them have pointer
+ * to the next node (*next), and pointer to pointer to data(**data), that stored in quantums. TODO: make it dynamically allocated
+ */
+struct scull_qset {
+void              ** data;       /* pointer to array of pointers to squll quantums */
+struct scull_qset *  p_next;     /* pointer to next node in linked list of scull_qset`s */
+struct scull_qset *  p_previous; /* pointer to previous node in linked list of scull_qset`s */
+}scull_qset;
 
 /*
  * struct scull_dev used for storing device numbers, transferring
@@ -24,13 +36,17 @@
  */
 
 struct scull_dev {
-dev_t          dev;            /* store big (12 bits) and little (20 bits) device number */
-unsigned int   number_opens;   /* number of device opens */
-char         * buffer;         /* pointer to character buffer for storing */
-ssize_t        buffer_size;    /* wrote bytes in buffer by characters */
+dev_t          dev;           /* store big (12 bits) and little (20 bits) device number */
+unsigned int   number_opens;  /* number of device opens */
+char         * buffer;        /* pointer to character buffer for storing */
+ssize_t        buffer_size;   /* wrote bytes in buffer by characters */
+unsigned int   qset_size;     /* # of nodes (squll_qsets) in scull linked list */
+unsigned int   quantum_size;  /* size in bytes of quantum */
+unsigned int   quantum_count; /* count of quantum pointers in 1 node in scull_qset`s linked list*/
+struct scull_qset * qset_head; /* pointer to linked list */
 }scull_dev;
 
 /* Uncomment for debug */
-/* #define SCULL_DEBUG */
+#define SCULL_DEBUG
 
 #endif /* SCULL_CONFIG_H */
